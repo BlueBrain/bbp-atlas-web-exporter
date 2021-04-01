d8ead1e279fd9d874034d02225ba09c5c6c1000a
jonathanlurie <jonathan.lurie@epfl.ch>
2021-04-01 15:36:05 +0200

    ADD export to RAB
    Change-Id: Ie645cff4a849e2e100359d798afbbb48acd4c3c4

--- sonata2rab/src/sonata2rab/readsonatapoints.py
+++ sonata2rab/src/sonata2rab/readsonatapoints.py
@@ -1,9 +1,12 @@
 import h5py
 import numpy as np
+import randomaccessbuffer as rab
+import pandas as pd
+
 
 
 GROUP_0_PATH = "nodes/atlas_cells/0"
-EXPECTED_DATASET_NAMES = {
+EXPECTED_MODALITY_NAMES = {
     "position": ["x", "y", "z"],
     "orientation": ["orientation_w", "orientation_x", "orientation_y", "orientation_z"],
     "region": ["region_id"],
@@ -16,9 +19,9 @@ def check_sonata(sonata_instance, raise_exception = True):
   group_0 = sonata_instance[GROUP_0_PATH]
   dataset_names = list(group_0.keys())
 
-  for category_name in EXPECTED_DATASET_NAMES:
-    for i in range(0, len(EXPECTED_DATASET_NAMES[category_name])):
-      name = EXPECTED_DATASET_NAMES[category_name][i]
+  for modality_name in EXPECTED_MODALITY_NAMES:
+    for i in range(0, len(EXPECTED_MODALITY_NAMES[modality_name])):
+      name = EXPECTED_MODALITY_NAMES[modality_name][i]
       if name not in dataset_names:
         if raise_exception:
           raise Exception(f"The dataset {name} is missing.")
@@ -30,36 +33,35 @@ def check_sonata(sonata_instance, raise_exception = True):
 
 def get_data(sonata_instance, dataset_name):
   dataset_path = f"{GROUP_0_PATH}/{dataset_name}"
-  return sonata_instance[dataset_path].value
+  return sonata_instance[dataset_path][()]
 
 
-def get_position(sonata_instance, downsampling = 1):
-  return {
-    "x": get_data(sonata_instance, "x")[::downsampling],
-    "y": get_data(sonata_instance, "y")[::downsampling],
-    "z": get_data(sonata_instance, "z")[::downsampling],
-  }
+def get_modality_data(sonata_instance, modality_name, downsampling = 1):
+  if modality_name not in EXPECTED_MODALITY_NAMES:
+    raise Exception(f"The modality {modality_name} does not exist.")
+
+  data = {}
+  for dataset_name in EXPECTED_MODALITY_NAMES[modality_name]:
+    data[dataset_name] = np.nan_to_num(get_data(sonata_instance, dataset_name)[::downsampling])
   
-
-# also replaces NaNs with zeros
-def get_orientation(sonata_instance, downsampling = 1):
-  return {
-    "orientation_x": np.nan_to_num(get_data(sonata_instance, "orientation_x")[::downsampling]),
-    "orientation_y": np.nan_to_num(get_data(sonata_instance, "orientation_y")[::downsampling]),
-    "orientation_z": np.nan_to_num(get_data(sonata_instance, "orientation_z")[::downsampling]),
-  }
+  return data
 
 
-def get_region_id(sonata_instance, downsampling = 1):
-  return {
-    "region_id": get_data(sonata_instance, "region_id")[::downsampling],
-  }
-
-
-def get_cell_type(sonata_instance, downsampling = 1):
-  return {
-    "cell_type": get_data(sonata_instance, "cell_type")[::downsampling],
-  }
+def modality_to_rab(sonata_instance, modality_name, output_file, downsampling = 1):
+  # getting the data
+  data = get_modality_data(sonata_instance, modality_name, downsampling)
+  # building a Pandas Dataframe out of the data
+  df = pd.DataFrame(data)
+  # instanciating a RandomAccessBuffer object
+  rab_instance = rab.RandomAccessBuffer()
+  # adding an index to lookup
+  rab_instance.addDataset("index", data = {
+    "type": "CellRecordSeries",
+    "modality": modality_name,
+  })
+  # adding the dataframe to the RAB
+  rab_instance.addDataset(modality_name, data = df, compress="gzip")
+  rab_instance.write(output_file)
 
 
 if __name__ == "__main__":
@@ -69,17 +71,7 @@ if __name__ == "__main__":
       print("This SONATA ain't no good.")
       exit()
 
-    position = get_position(sonata)
-    orientation = get_orientation(sonata)
-    region_id = get_region_id(sonata)
-    cell_type = get_cell_type(sonata)
-    
-    print(position)
-    print(orientation)
-    print(region_id)
-    print(cell_type)
-
-
-# print( group_0["x"] )
-
-# xpos = np.float32(h5file["x"].value)
+    modality_to_rab(sonata, "position", "/Users/lurie/Documents/BBP/projects/blue_brain_atlas_website_exporter/sonata2rab/sample_data/position.rab")
+    modality_to_rab(sonata, "orientation", "/Users/lurie/Documents/BBP/projects/blue_brain_atlas_website_exporter/sonata2rab/sample_data/orientation.rab")
+    modality_to_rab(sonata, "region", "/Users/lurie/Documents/BBP/projects/blue_brain_atlas_website_exporter/sonata2rab/sample_data/region.rab")
+    modality_to_rab(sonata, "cell_type", "/Users/lurie/Documents/BBP/projects/blue_brain_atlas_website_exporter/sonata2rab/sample_data/cell_type.rab")
