3301c32e2db1f5b3d745de64dfbdbad115d22fd3
jonathanlurie <jonathan.lurie@epfl.ch>
2021-08-18 13:29:37 +0200

    ENH version number is now dependent on git

--- src/blue_brain_atlas_web_exporter/__init__.py
+++ src/blue_brain_atlas_web_exporter/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "0.1.0"
+"""
+Get the actual version of the package as it is installed
+that depends on the git tag. Gives the same as 'pip show'.
+
+(As opposed as a hardcoded version number that the 
+developer has to maintain)
+"""
+
+import pkg_resources
+# This is the name of your current package itself
+from blue_brain_atlas_web_exporter import __name__
+__version__ = pkg_resources.get_distribution(__name__).version
